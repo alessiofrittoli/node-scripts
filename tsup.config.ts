fe7a11e6f3eff36dea08b131df7dd8fc0eb267a6
Alessio Frittoli <info@alessiofrittoli.it>
2024-12-25 00:35:29 +0100

    chore: update package.json and tsup.config.ts to use .js extensions

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,15 +4,10 @@ export default defineConfig( {
 	entry		: [ 'src/index.ts', 'src/release.ts', 'src/postinstall/index.ts' ],
 	format		: [ 'cjs', 'esm' ],
 	dts			: true,
-	splitting	: true,
+	splitting	: false,
 	shims		: true,
 	skipNodeModulesBundle: true,
 	clean		: true,
 	treeshake	: true,
 	minify		: true,
-	esbuildOptions( options, { format } ) {
-		if ( format !== 'cjs' ) return options
-		options.outExtension = { '.js': '.cjs' }
-		return options
-	},
 } )
