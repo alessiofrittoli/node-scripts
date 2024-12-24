c25c46b71da6dea70fd979681ddfc41460d200f3
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2024-12-24 20:09:50 +0100

    refactor: update exports to use named exports for consistency (#13)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from './postinstall'
 export * from './git'
 export * from './package'
 export * from './process'
-export { default as release } from './release'
+export * from './release'
 
 export type * from './types'
